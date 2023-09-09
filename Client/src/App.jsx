c9d400a469c99004ad61238bce015b3762591bdc
braiaguirre <brianaguirre.sp94@gmail.com>
2023-09-09 12:55:35 -0300

    error popup 100%

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,81 +10,69 @@ import Error404 from './views/Error404/Error404.jsx';
 import About from './views/About/About.jsx';
 
 // COMPONENTS
-import Nav from './components/Nav/Nav.jsx'
+import Nav from './components/Nav/Nav.jsx';
+import ErrorPopup from './components/ErrorPopup/ErrorPopup.jsx';
 
 // DEPENDENCIES
-import {useState, useEffect} from 'react';
+import {useEffect} from 'react';
 import {Route, Routes, useNavigate} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
+
+// ACTIONS
 import {getCharacter, removeCharacter, clearError, getAccess, removeAccess} from './redux/actions/actions.js';
 
-export default function App() {
-   const [mem, setMem] = useState([]);
-   // const [access, setAccess] = useState(true);
-   const access = useSelector(state => state.access);
-   const characters = useSelector(state => state.allCharacters);
-   const error = useSelector(state => state.error);
+function App() {
    const navigate = useNavigate();
    const dispatch = useDispatch();
 
-   // ACCESS - LOGIN - LOGOUT
-   function logIn({email, password}) {
-      dispatch(getAccess(email, password));
-   }
-   
-   function logOut() {
-      dispatch(removeAccess);
-   }
+   const access = useSelector(state => state.access);
+   const characters = useSelector(state => state.allCharacters);
+   const error = useSelector(state => state.error);
+
+   // ACCESS HANDLERS
+
+   const logIn = ({email, password}) => dispatch(getAccess(email, password));
+   const logOut = () => dispatch(removeAccess);
 
    useEffect(() => {
-      // !access && navigate('/');
       access ? navigate('/home') : navigate('/')
    }, [access]);
-
-   useEffect(() => {
-      if (error !== '') alert(error);
-   }, [error])
    
-   // ADD CHARACTER
-   function onSearch(id) {
-      dispatch(clearError); // --------------- > MEJORAR
-      dispatch(getCharacter(id));
-      setMem(mem => [...mem, Number(id)]);
-   }
+   // CHARACTERS HANDLERS
 
-   // REMOVE CHARACTER
-   function onClose(id = -1) {
-      if (id === -1) {
-         dispatch(removeCharacter(id));
-         setMem([]);
-      } else {
-         dispatch(removeCharacter(id));
-         setMem(mem.filter(character => character.id !== Number(id)));
-      }
-   }
+   const onSearch = (id) => dispatch(getCharacter(id));
+   const onClose = (id = -1) => dispatch(removeCharacter(id));
 
-   // APP
    return (
       <>
-         {access && <div className={styles.navbar}>
-            <Nav onSearch={onSearch} logOut={logOut} onClose={onClose} />
-         </div>}
+         {error !== '' && 
+            <div className={styles.errorPopupContainer}>
+               <ErrorPopup errorMessage={error} />
+            </div>}
+         {access && 
+            <div className={styles.navbar}>
+               <Nav onSearch={onSearch} logOut={logOut} onClose={onClose} />
+            </div>}
          <>
-            {!access && <div className={styles.login}>
-               <Routes>
-                  <Route path='/' element={<Login logIn={logIn}/>} />
-               </Routes>
-            </div>}
-            {access && <div className={styles.app}>
-               <Routes>
-                  <Route path='/home' element={<Home characters={characters} onClose={onClose} />} />
-                  <Route path='/favorites' element={<Favorites />} />
-                  <Route path='/about' element={<About />} />
-                  <Route path='/detail/:id' element={<Detail />}  />
-                  <Route path='*' element={<Error404 />} />
-               </Routes>
-            </div>}
+            {!access && 
+               <div className={styles.login}>
+                  <Routes>
+                     <Route path='/' element={<Login logIn={logIn}/>} />
+                  </Routes>
+               </div>}
+            {access && 
+               <div className={styles.app}>
+                  <Routes>
+                     <Route path='/home' element={<Home characters={characters} onClose={onClose} />} />
+                     <Route path='/favorites' element={<Favorites />} />
+                     <Route path='/about' element={<About />} />
+                     <Route path='/detail/:id' element={<Detail />}  />
+                     <Route path='*' element={<Error404 />} />
+                  </Routes>
+               </div>}
          </>
       </>
    );
 }
+
+export default App;
--- Client/src/components/ErrorPopup/ErrorPopup.jsx
+++ Client/src/components/ErrorPopup/ErrorPopup.jsx
@@ -1,2 +1,26 @@
+// STYLES
+import styles from './ErrorPopup.module.css';
+
+// DEPENDENCIES
 import React from 'react';
-import Popup from 'reactjs-popup';
+import {useDispatch} from 'react-redux';
+
+// ACTIONS
+import {clearError} from '../../redux/actions/actions.js';
+
+function ErrorPopup({errorMessage}) {
+    const dispatch = useDispatch();
+
+    function clearErrorHandler() {
+        dispatch(clearError());
+    }
+    
+    return (
+        <div className={styles.errorPopup}>
+            <h2>{errorMessage}</h2>
+            <button onClick={clearErrorHandler}>Close</button>
+        </div>
+    )
+}
+
+export default ErrorPopup;
