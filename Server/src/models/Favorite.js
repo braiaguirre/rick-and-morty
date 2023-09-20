55d89eb577100a1c3c4bd48986d68ed4e9a264c7
braiaguirre <brianaguirre.sp94@gmail.com>
2023-09-20 15:23:40 -0300

    server update

--- Server/src/models/Favorite.js
+++ Server/src/models/Favorite.js
@@ -1,5 +1,35 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-   sequelize.define('Favorite', {}, { timestamps: false });
+   sequelize.define('Favorite', {
+      id: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         primaryKey: true
+      },
+      name: {
+         type: DataTypes.STRING,
+         allowNull: false
+      },
+      status: {
+         type: DataType.ENUM('Alive', 'Dead', 'unknown'),
+         allowNull: false
+      },
+      species: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      gender: {
+         type: DataType.ENUM('Female', 'Male', 'Genderless', 'unknown'),
+         allowNull: false
+      },
+      origin: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      image: {
+         type: DataType.STRING,
+         allowNull: false
+      }
+   }, { timestamps: false });
 };
