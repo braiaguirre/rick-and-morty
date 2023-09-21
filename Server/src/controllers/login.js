9260e39b6bfc72ddf47c94edfb71de712ab5c7d5
braiaguirre <brianaguirre.sp94@gmail.com>
2023-09-21 15:01:55 -0300

    update

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -5,19 +5,11 @@ const login = async (req, res) => {
     try {
         const {email, password} = req.query;
         if (email === '' || password === '') return res.status(400).send('Empty data');
-
-        const user = await User.findOne({
-            where: {
-                email
-            }
-        });
-
+        const user = await User.findOne({where: {email}});
         if (!user) return res.status(404).send('Not found');
-        return user.password === password ?
-        res.status(200).json({access: true})
-        : res.status(403).send('Incorrect password');
+        user.password === password ? res.status(200).json({access: true}) : res.status(403).send('Incorrect password');
     } catch(err) {
-        return res.status(500).send(err.message);
+        res.status(500).send(err.message);
     }
     
 }
--- Server/src/controllers/postUser.js
+++ Server/src/controllers/postUser.js
@@ -10,9 +10,9 @@ const postUser = async (req, res) => {
                 password
             }
         });
-        return created ? res.status(200).json(user) : res.status(400).send('Already exists');
+        created ? res.status(200).json(user) : res.status(400).send('Already exists');
     } catch (error) {
-        return res.status(404).json({error: error.message});
+        res.status(404).json({error: error.message});
     }
 }
 
