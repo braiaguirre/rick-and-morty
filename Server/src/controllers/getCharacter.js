416bd4c44d1653601141bf97dea68c5b4081a752
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-20 08:21:00 -0300

    update

--- Server/src/controllers/getCharacter.js
+++ Server/src/controllers/getCharacter.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 const URL = 'https://rickandmortyapi.com/api/character/';
 
 async function getCharacter(req, res) { // TODO: PROMISE ALL 
-    console.log('getCharacter.js')
     try {
         const {id} = req.params;
         const {name, gender, species, origin, status} = req.query;
